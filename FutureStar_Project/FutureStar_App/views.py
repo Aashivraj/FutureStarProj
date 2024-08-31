932d676d5dc754808c76679252eb80083d8377ad
Devansh1312 <shahdevansh13122002@gmail.com>
2024-08-31 11:53:23 +0530

    System Settings

--- FutureStar_Project/FutureStar_App/views.py
+++ FutureStar_Project/FutureStar_App/views.py
@@ -4,7 +4,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .forms import LoginForm, UserForm, RoleForm, CategoryForm, SystemSettingsForm
 from .models import User, Role, Category, SystemSettings
-
+import os
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Login View
 class LoginView(View):
@@ -192,21 +195,71 @@ class CategoryListView(View):
 
 
 # System Settings View (Assuming there's only one instance)
-class SystemSettingsUpdateView(View):
-    template_name = 'system_settings_form.html'
+class System_Settings(LoginRequiredMixin,View):
+    login_url = '/'
+    redirect_field_name = 'redirect_to'
 
-    def get(self, request):
-        settings = SystemSettings.objects.first()
-        form = SystemSettingsForm(instance=settings)
-        return render(request, self.template_name, {'form': form})
+    def get(self, request, *args, **kwargs):
+        system_settings = SystemSettings.objects.first()  # Fetch the first record
+        return render(request, 'Admin/System_Settings.html', {
+            'system_settings': system_settings,
+            'MEDIA_URL': settings.MEDIA_URL,  # Pass MEDIA_URL to the template
+        })
 
-    def post(self, request):
-        settings = SystemSettings.objects.first()
-        form = SystemSettingsForm(request.POST, instance=settings)
-        if form.is_valid():
-            form.save()
-            return redirect('system_settings')
-        return render(request, self.template_name, {'form': form})
+    def post(self, request, *args, **kwargs):
+        system_settings = SystemSettings.objects.first()
+        if not system_settings:
+            system_settings = SystemSettings()
+
+        fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'System_Settings'))
+
+        # Handle fav_icon
+        if 'fav_icon' in request.FILES:
+            if system_settings.fav_icon:
+                old_fav_icon_path = os.path.join(settings.MEDIA_ROOT, system_settings.fav_icon)
+                if os.path.isfile(old_fav_icon_path):
+                    os.remove(old_fav_icon_path)
+            fav_icon_file = request.FILES['fav_icon']
+            fav_icon_filename = 'favicon.jpg'
+            fs.save(fav_icon_filename, fav_icon_file)
+            system_settings.fav_icon = os.path.join('System_Settings', fav_icon_filename)
+
+        # Handle footer_logo
+        if 'footer_logo' in request.FILES:
+            if system_settings.footer_logo:
+                old_footer_logo_path = os.path.join(settings.MEDIA_ROOT, system_settings.footer_logo)
+                if os.path.isfile(old_footer_logo_path):
+                    os.remove(old_footer_logo_path)
+            footer_logo_file = request.FILES['footer_logo']
+            footer_logo_filename = 'footer_logo.jpg'
+            fs.save(footer_logo_filename, footer_logo_file)
+            system_settings.footer_logo = os.path.join('System_Settings', footer_logo_filename)
+
+        # Handle header_logo
+        if 'header_logo' in request.FILES:
+            if system_settings.header_logo:
+                old_header_logo_path = os.path.join(settings.MEDIA_ROOT, system_settings.header_logo)
+                if os.path.isfile(old_header_logo_path):
+                    os.remove(old_header_logo_path)
+            header_logo_file = request.FILES['header_logo']
+            header_logo_filename = 'header_logo.jpg'
+            fs.save(header_logo_filename, header_logo_file)
+            system_settings.header_logo = os.path.join('System_Settings', header_logo_filename)
+
+        # Save other fields
+        system_settings.website_name_english = request.POST.get('website_name_english')
+        system_settings.website_name_arabic = request.POST.get('website_name_arabic')
+        system_settings.phone = request.POST.get('phone')
+        system_settings.email = request.POST.get('email')
+        system_settings.address = request.POST.get('address')
+        system_settings.instagram = request.POST.get('instagram')
+        system_settings.facebook = request.POST.get('facebook')
+        system_settings.snapchat = request.POST.get('snapchat')
+        system_settings.linkedin = request.POST.get('linkedin')
+        system_settings.youtube = request.POST.get('youtube')
+
+        system_settings.save()
+        return redirect('Dashboard')
 
 # Error 404 html
 class ErrorView(View):
